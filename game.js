47774f039a7964b3dad99a0468dab06dc5673b2d
Zzodiaq <101874681+Zzodiaq@users.noreply.github.com>
2022-08-30 16:11:44 +0200

    Add files via upload

--- game.js
+++ game.js
@@ -4,18 +4,23 @@ userClickedPattern = [];
 
 
 
+   $("*").one("keypress", function(){
+      nextSequence();
+   });
+
+
 function nextSequence(){
    const randomNumber = Math.floor(Math.random() * (3 - 0 + 1));
    const randomChosenColour = buttonColours[randomNumber];
    gamePattern.push(randomChosenColour);
    $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-  
+  console.log(randomNumber);
    $(".btn").on('click', function(){
       const userChosenColour = $(this).attr("id");
       userClickedPattern.push(userChosenColour);
       playSound(userClickedPattern);
+      animatePress(userClickedPattern);
    });
-   
 }
 
 
@@ -25,7 +30,16 @@ function playSound(name){
    audio.play();
 }
 
-nextSequence();
+
+
+function animatePress(currentColour){
+   $('#' + currentColour).addClass("pressed");
+   setTimeout(function(){
+      $('#' + currentColour).removeClass("pressed");
+   }, 100);
+}
+
+
 
 
 
